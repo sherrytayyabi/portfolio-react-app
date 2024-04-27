fe1f31339e1d8d57001adf7deab581710eb36c23
sherrytayyabi <sherry.tayyabi@gmail.com>
2024-04-27 02:15:21 -0400

    Navbar width changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     return (
         <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
             <div>
-                <img src={Logo} alt="Logo Image" style={{ height: '80px' }} />
+                <img src={Logo} alt="Logo Image" style={{ height: '80px', width: '220px' }} />
             </div>
 
             {/*Menu */}
